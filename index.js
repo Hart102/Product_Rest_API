9a37f2e71266bd6ce29f9afef4a0f282d6805861
Hart102 <hartongit102@gmail.com>
2025-03-17 13:13:24 -0600

    added swagger url

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({
-  origin: "*",
+  origin: "https://product-rest-api-dqv9.onrender.com/",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: "Content-Type, Authorization"
 }));
